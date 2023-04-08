d4aee54870a1de111da368adb0d316870fd2c068
MakarovPetr <125215189+MakarovPetr@users.noreply.github.com>
2023-04-08 12:17:26 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,13 +1,13 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template <typename T, int size>
 class TStack {
  private:
   int top;
   T arr[100];
-  
  public:
   TStack() : top(-1) {}
   void push(T value) {
